8a71823725ec8db91185172e7aaa21e673fc7840
karishma-ghanwat <ghanwatkarishma9@gmail.com>
2026-07-30 18:22:18 +0530

    Updated API base URL for production deployment

--- Frontend/payflow-frontend/src/services/api.js
+++ Frontend/payflow-frontend/src/services/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL:
+        import.meta.env.MODE === "development"
+            ? "http://localhost:5000/api"
+            : "https://payflow-29rg.onrender.com/api",
 });
 
-// ✅ Attach token automatically
+// ✅ Attach JWT automatically
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
 
